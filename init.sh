f7c5ddd0f2f3a24fda9139263a2753fb32da11e4
Cyanose <KrzysztofPastuszka@protonmail.com>
2022-08-20 13:38:41 +0200

    added init script

---
+++ init.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+sqlite3 database.db "CREATE TABLE food (record INTEGER PRIMARY KEY AUTOINCREMENT, \
+					name text, \
+					kcal REAL DEFAULT 0, \
+					carbs REAL DEFAULT 0, \
+					proteins REAL DEFAULT 0, \
+					fats REAL DEFAULT 0);"
+
+sqlite3 database.db "CREATE TABLE diary (record INTEGER PRIMARY KEY AUTOINCREMENT, \
+					 day TEXT, \
+					 amount REAL, \
+					 name TEXT, \
+					 kcal REAL, \
+					 carbs REAL, \
+					 proteins REAL, \
+					 fats REAL);"
+
+sqlite3 database.db "CREATE TABLE food_servings (own_record INTEGER PRIMARY KEY AUTOINCREMENT, \
+					         reference_record INTEGER, \
+						 serving_name TEXT, \
+						 serving_size REAL DEFAULT 0, \
+						 FOREIGN KEY(reference_record) REFERENCES food(record));"
+
+sqlite3 database.db "CREATE TABLE colors (color TEXT, n INT, fmt TEXT);"
